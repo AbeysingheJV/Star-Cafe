caa7cfa3d4d84a7d058710fc411138273444a471
AbeysingheJV <januda2015@gmail.com>
2025-04-30 17:23:29 +0530

    changed the cooking system

--- Assets/Scripts/Player/PlayerInputHandler.cs
+++ Assets/Scripts/Player/PlayerInputHandler.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Events;
+
 
 public class PlayerInputHandler : MonoBehaviour
 {
@@ -8,48 +8,43 @@ public class PlayerInputHandler : MonoBehaviour
 	[SerializeField] private InputActionAsset playerControls;
 
 	[Header("Action Map Name Reference")]
-	[SerializeField] private string actionMapName = "Player";
+	[SerializeField] private string actionMapName = "Player"; 
 
 	[Header("Action Name References")]
 	[SerializeField] private string movement = "Movement";
-	[SerializeField] private string rotation = "Rotation";
-	[SerializeField] private string interact = "Interact"; 
-	[SerializeField] private string cookAction = "CookAction"; 
+	[SerializeField] private string rotation = "Rotation"; 
+														   
 
 	private InputAction movementAction;
 	private InputAction rotationAction;
-	private InputAction interactAction;
-	private InputAction cookActionInput; 
+	
 
 	public Vector2 MovementInput { get; private set; }
 	public Vector2 RotationInput { get; private set; }
 
+	
 
-	public UnityAction OnInteractActionStarted;
-	public UnityAction OnCookActionStarted; 
-	public UnityAction OnCookActionCanceled; 
-
-	private void Awake()
+	private void Awake() 
 	{
-		InputActionMap mapReference = playerControls.FindActionMap(actionMapName); 
+		InputActionMap mapReference = playerControls.FindActionMap(actionMapName);
 
-        if (mapReference == null)
+		if (mapReference == null)
 		{
 			Debug.LogError($"Action Map '{actionMapName}' not found!");
 			enabled = false;
-			return;
+			return; 
 		}
 
-		movementAction = mapReference.FindAction(movement); 
-        rotationAction = mapReference.FindAction(rotation); 
-        interactAction = mapReference.FindAction(interact); 
-        cookActionInput = mapReference.FindAction(cookAction); 
+		movementAction = mapReference.FindAction(movement);
+		rotationAction = mapReference.FindAction(rotation);
+		
 
 		
-		if (movementAction == null || rotationAction == null || interactAction == null || cookActionInput == null) 
-        {
-			Debug.LogError("One or more Input Actions not found! (Movement, Rotation, Interact, CookAction)"); 
-            enabled = false;
+		if (movementAction == null || rotationAction == null) 
+		{
+			
+			Debug.LogError("One or more Input Actions not found! (Movement, Rotation)");
+			enabled = false; 
 			return;
 		}
 
@@ -59,61 +54,25 @@ public class PlayerInputHandler : MonoBehaviour
 	private void SubscribeActionValuesToInputEvents()
 	{
 		// Movement
-		movementAction.performed += ctx => MovementInput = ctx.ReadValue<Vector2>(); 
-        movementAction.canceled += ctx => MovementInput = Vector2.zero; 
+		movementAction.performed += ctx => MovementInput = ctx.ReadValue<Vector2>();
+		movementAction.canceled += ctx => MovementInput = Vector2.zero; 
 
-        // Rotation
-        rotationAction.performed += ctx => RotationInput = ctx.ReadValue<Vector2>(); 
-        rotationAction.canceled += ctx => RotationInput = Vector2.zero;
+		// Rotation
+		rotationAction.performed += ctx => RotationInput = ctx.ReadValue<Vector2>();
+		rotationAction.canceled += ctx => RotationInput = Vector2.zero; 
 
-        // Interact (E)
-        interactAction.started += InteractStarted; 
-												   
-
-		// CookAction (Q) 
-		cookActionInput.started += CookActionStarted; 
-        cookActionInput.canceled += CookActionCanceled; 
-    }
-
-	private void InteractStarted(InputAction.CallbackContext context)
-	{
-		OnInteractActionStarted?.Invoke(); 
-    }
-
-
-	private void CookActionStarted(InputAction.CallbackContext context)
-	{
-		OnCookActionStarted?.Invoke(); 
-    }
-
-	private void CookActionCanceled(InputAction.CallbackContext context)
-	{
 		
-    }
+	}
 
+	
 
 	private void OnEnable()
 	{
 		playerControls.FindActionMap(actionMapName).Enable(); 
-    }
+	}
 
 	private void OnDisable()
 	{
-		// Unsubscribe Interact
-		if (interactAction != null)
-		{
-			interactAction.started -= InteractStarted; 
-            // interactAction.canceled -= InteractCanceled;
-        }
-
-		// Unsubscribe CookAction 
-		if (cookActionInput != null)
-		{
-			cookActionInput.started -= CookActionStarted;
-			cookActionInput.canceled -= CookActionCanceled;
-		}
-
-
-		playerControls.FindActionMap(actionMapName).Disable(); 
-    }
+		playerControls.FindActionMap(actionMapName).Disable();
+	}
 }
